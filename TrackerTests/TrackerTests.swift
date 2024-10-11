cd4313e6e74fa284cdd0a1203946bd649ad7182b
ZFAbox <starbreeze@yandex.ru>
2024-10-11 00:11:54 -0700

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Fedor on 11.10.2024.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
